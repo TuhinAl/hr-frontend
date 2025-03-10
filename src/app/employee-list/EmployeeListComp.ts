b2cfbfbfc08cfbe578fa8337d2c61b75b668f357
tuhinal <alauddintuhin1411@gmail.com>
2025-03-10 08:08:39 +0600

    modified employee list components

--- src/app/employee-list/EmployeeListComp.ts
+++ src/app/employee-list/EmployeeListComp.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { ColDef } from "ag-grid-community";
+import { EmployeeInfoSearchDto } from "../dto/EmployeeInfoSearchDto";
+import { EmployeeListCompService } from "./EmployeeListCompService";
+import { Page } from "../common/model/page";
+import { EmployeeInfoDto } from "../dto/EmployeeInfoDto";
+import { CurrentPage } from "../common/model/current-page";
+import { ApiResponse } from "../common/util/ApiResponse";
 
 @Component({
   selector: 'EmployeeListComp',
@@ -7,10 +14,40 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EmployeeListComp implements OnInit{
   
-  ngOnInit() {
-    
+  columnDef: ColDef[] = [
+    {headerName: 'Employee Name', field: 'firstName', width: 250, editable: false, colId: 'firstName', filter: true},
+    {headerName: 'Employee ID', field: 'employeeNcId', width: 250, editable: false, colId: 'employeeNcId', filter: true},
+    {headerName: 'Email', field: 'email', width: 200, editable: false, colId: 'email', filter: true},
+  ];
+  id: string | null = null
+  employeeInfoDtoPage: Page<EmployeeInfoDto> = new Page()
+
+  
+  constructor(private employeeListCompService : EmployeeListCompService) { 
+   this.id = localStorage.getItem('id');
   }
 
+  ngOnInit() {
+    this.searchEmployee()
+  }
 
+  searchEmployee() {
+    const employeeSearch: EmployeeInfoSearchDto = new EmployeeInfoSearchDto();
+    employeeSearch.page = 0;
+    employeeSearch.size = 10;
+    employeeSearch.id = this.id
+    this.employeeListCompService.search(employeeSearch)
+      .subscribe((res: ApiResponse<Page<EmployeeInfoDto>> | null) => {
+        if (res && res.data.content) {
+          this.employeeInfoDtoPage = res.data;
+          this.priintEmployeeInfo(this.employeeInfoDtoPage);
+        }
+      });
+
+  }
+
+priintEmployeeInfo(data: Page<EmployeeInfoDto>){
+console.log(data);
+}
 
 }
--- src/app/employee-list/EmployeeListCompService.ts
+++ src/app/employee-list/EmployeeListCompService.ts
@@ -1,6 +1,12 @@
-import {Injectable} from "@angular/core";
-import {EmployeeApiService} from "../common/service/EmployeeApiService";
-import {ToastrService} from "ngx-toastr";
+import { Injectable } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { catchError, Observable } from "rxjs";
+import { Page } from "../common/model/page";
+import { EmployeeApiService } from "../common/service/EmployeeApiService";
+import { ApiResponse } from "../common/util/ApiResponse";
+import { customErrorHandler } from "../common/util/ErrorMessageHandler";
+import { EmployeeInfoDto } from "../dto/EmployeeInfoDto";
+import { EmployeeInfoSearchDto } from "../dto/EmployeeInfoSearchDto";
 
 @Injectable()
 export class EmployeeListCompService {
@@ -8,5 +14,12 @@ export class EmployeeListCompService {
   constructor(private employeeApiService: EmployeeApiService,
               private notify: ToastrService) {
   }
+
+  search(searchDto: EmployeeInfoSearchDto): Observable<ApiResponse<Page<EmployeeInfoDto>> | null> {
+    return this.employeeApiService.search(searchDto).pipe(
+      catchError(e => customErrorHandler(e, this.notify)),
+    )
+  }
+
 
 }
